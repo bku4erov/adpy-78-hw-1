7a2244313a2300db6ff0d01f13279cd8d3cb07ef
Boris Kucherov <boris.ku4erov@gmail.com>
2023-07-08 17:27:31 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import datetime as dt
+
+from application.db.people import get_employees
+from application.salary import calculate_salary
+
+if __name__ == '__main__':
+    print(f'Today is {dt.date.today()}')
+    calculate_salary()
+    get_employees()
+    
